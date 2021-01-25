7b3e31538bd111fb187e5601be4db8857cb9c549
EidAbdullahi <eidabdullahi10@gmail.com>
2021-01-25 10:08:08 +0300

    commit all changes

--- appawards/models.py
+++ appawards/models.py
@@ -34,13 +34,13 @@ class Profile(models.Model):
         instance.profile.save()
 
 class Projects(models.Model):
-    # project_name = models.CharField(max_length=50, blank=True)
+    project_name = models.CharField(max_length=50, blank=True)
+    project_name = models.CharField(max_length=50, blank=True)
     project_photo = CloudinaryField('projectpics/')
     description = models.TextField(max_length=400,blank=True)
     github_repo = models.CharField(max_length=150, blank=True)
     url = models.CharField(max_length=60,blank=True)
     uploader = models.ForeignKey(User, null=True,on_delete=models.CASCADE)
-    project_name = models.CharField(max_length=50, blank=True)
-
+    
     def __str__(self):
         return self.url
